052e3b70d5ed2ddaed3fe7e087f5d00d14b9b03b
Brad Broge <brbroge@gmail.com>
2017-09-04 12:49:06 -0400

    significant updates to the header, some cart modifications

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,6 @@ class Header extends Component {
             <UserTypeDropdown {...this.props}/>
           </header>
         )
-      break;
       case true:
         switch (userType) {
           case "user":
@@ -35,21 +34,24 @@ class Header extends Component {
                <div className="branding"></div>
                <nav className="top-nav" >
                  <ul id="main-menu" className="menu-ul" style={{listStyleType: 'none', display: 'flex', justifyContent: 'center', padding: '20px'}}>
-                   <li className="menu-item"><NavLink to="/user/productlist" style={{textDecoration: 'none', margin:'50px', color: "black"}}>Main</NavLink></li>
-                   <li className="menu-item"><NavLink to="/user/productlist/:type" style={{textDecoration: 'none', margin:'50px', color: "black"}}>Heroes</NavLink></li>
-                   <li className="menu-item"><NavLink to="/user/productlist/:type" style={{textDecoration: 'none', margin:'50px', color: "black"}}>Villains</NavLink></li>
+                   <li className="menu-item"><NavLink to="/productlist" style={{textDecoration: 'none', margin:'50px', color: "black"}}>Main</NavLink></li>
+                   {/*
+                     <li className="menu-item"><NavLink to="/user/productlist/:type" style={{textDecoration: 'none', margin:'50px', color: "black"}}>Heroes</NavLink></li>
+                     <li className="menu-item"><NavLink to="/user/productlist/:type" style={{textDecoration: 'none', margin:'50px', color: "black"}}>Villains</NavLink></li>
+                   */}
                  </ul>
                </nav>
                <nav className="user-menu">
                  <ul id="user-main-menu" className="menu-ul"style={{listStyleType: 'none', display: 'flex', justifyContent: 'center', padding: '20px'}} >
                    <li className="menu-item"><NavLink to="/"><i className="fa fa-user" aria-hidden="true" style={{marginRight:"30px", color: "black"}}></i></NavLink></li>
-                   <li className="menu-item"><NavLink to="/"><i className="fa fa-shopping-cart" aria-hidden="true" style={{marginRight:"2px", color: "black"}}></i></NavLink></li>
-                   <span className="amount" style={{fontFamily: "Roboto", fontSize: 18, padding: "8px", marginLeft: "0px"}}><span className="currencySymbol">$</span>0.00</span>
+                   <li className="menu-item"><NavLink to="/shoppingCart"><i className="fa fa-shopping-cart" aria-hidden="true" style={{marginRight:"2px", color: "black"}}></i></NavLink></li>
+                   {/*
+                     <span className="amount" style={{fontFamily: "Roboto", fontSize: 18, padding: "8px", marginLeft: "0px"}}><span className="currencySymbol">$</span>0.00</span>
+                    */}
                  </ul>
                </nav>
              </header>
             )
-          break;
           case "admin":
             return (
               <header id="header" className="header-transparent" style={{fontSize: 36, display: 'flex', justifyContent: 'space-between', opacity: '100%', top:'0', width: '100%'}}>
@@ -57,15 +59,20 @@ class Header extends Component {
                 <nav className="top-nav" >
                   <ul id="main-menu" className="menu-ul" style={{listStyleType: 'none', display: 'flex', justifyContent: 'center', padding: '20px'}}>
                     <li className="menu-item"><NavLink to="/" style={{textDecoration: 'none', margin:'50px', color: "black"}}>Home</NavLink></li>
-                    <li className="menu-item"><NavLink to="/" style={{textDecoration: 'none', margin:'50px', color: "black"}}>Heroes</NavLink></li>
-                    <li className="menu-item"><NavLink to="/" style={{textDecoration: 'none', margin:'50px', color: "black"}}>Villains</NavLink></li>
+                    {/*
+                      <li className="menu-item"><NavLink to="/" style={{textDecoration: 'none', margin:'50px', color: "black"}}>Heroes</NavLink></li>
+                      <li className="menu-item"><NavLink to="/" style={{textDecoration: 'none', margin:'50px', color: "black"}}>Villains</NavLink></li>
+                    */}
                   </ul>
                 </nav>
                 <Field onClick={this.ReqForm(this.props.reqForm)}  name="addProduct" className="addProduct" component="button" value="adding"><Link to="/productlist/newProduct">Add Product</Link></Field>
               </header>
             )
-          // break;
+          default:
+            return null;
         }
+      default:
+        return null;
     }
   }
 }
--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -12,33 +12,35 @@ class CartItem extends Component {
     // });
     let cartProducts = cart.map(product => {
       let itemTotal = product.subtotal;
-      let cartItemKey = product.product.title + "_" + product.service;
-      return <div>
-        <div key={cartItemKey} className="cartCardStyle">
-          <div className="cartImageStyle">
-            <img className="thumbnailImage" src={product.product.imgSrc} alt={"portrait of " + product.product.title}/>
-          </div>
-          <div className="cartItemInfo">
-            <header>
-              <h2>{product.product.title}</h2>
-              {/* <h4>{product.type}</h4>
-              <h3>{product.tagline}</h3> */}
-            </header>
-            <Select {...this.props}/>
-            <div>
-              Service: {product.service}
+      let cartItemKey = product.title + "_" + Math.floor(Math.random()*10000);
+      return (
+        <div key={cartItemKey}>
+          <div className="cartCardStyle">
+            <div className="cartImageStyle">
+              <img className="thumbnailImage" src={product.product.imgSrc} alt={"portrait of " + product.product.title}/>
             </div>
-            <div className="cartCardHours">
-              {/* <button value="-" onClick={this.decrementHours}>-</button> */}
-              Hours: {product.hours}
-              {/* <button value="+" onClick={this.incrementHours}>+</button> */}
-            </div>
-            <div className="cartCardTotal">
-              Item Total: {itemTotal}
+            <div className="cartItemInfo">
+              <header>
+                <h2>{product.product.title}</h2>
+                {/* <h4>{product.type}</h4>
+                <h3>{product.tagline}</h3> */}
+              </header>
+              <Select {...this.props}/>
+              <div>
+                Service: {product.service}
+              </div>
+              <div className="cartCardHours">
+                {/* <button value="-" onClick={this.decrementHours}>-</button> */}
+                Hours: {product.hours}
+                {/* <button value="+" onClick={this.incrementHours}>+</button> */}
+              </div>
+              <div className="cartCardTotal">
+                Item Total: {itemTotal}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )
     })
 
     return (
